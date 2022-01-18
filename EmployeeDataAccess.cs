9cc811c0e33f8b83e49d911d02d1f5316ba26781
cr1s71na <50423083+cr1s71na@users.noreply.github.com>
2022-01-18 17:38:21 +0000

    Add files via upload

---
+++ EmployeeDataAccess.cs
@@ -0,0 +1,88 @@
+﻿using InterviewTest.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InterviewTest.Model
+{
+    public class EmployeeDataAccess
+    {
+        EmployeeContext db = new EmployeeContext();
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            try
+            {
+                return db.Employee.ToList();
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Add new employee record     
+        public int AddEmployee(Employee employee)
+        {
+            try
+            {
+                db.Employee.Add(employee);
+                db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Update the records of a particluar employee    
+        public int UpdateEmployee(Employee employee)
+        {
+            try
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //Get the details of a particular employee    
+        public Employee GetEmployeeData(int id)
+        {
+            try
+            {
+                Employee employee = db.Employee.Find(id);
+                return employee;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //To Delete the record of a particular employee    
+        public int DeleteEmployee(int id)
+        {
+            try
+            {
+                Employee emp = db.Employee.Find(id);
+                db.Employee.Remove(emp);
+                db.SaveChanges();
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
---
+++ EmployeesController.cs
@@ -0,0 +1,75 @@
+﻿using InterviewTest.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace InterviewTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmployeesController : ControllerBase
+    {
+        EmployeeDataAccess objemployee = new EmployeeDataAccess();
+
+        [HttpGet]
+        [Route("api/Employee/Index")]
+        public IEnumerable<Employee> Index()
+        {
+            return objemployee.GetAllEmployees();
+        }
+
+        [HttpPost]
+        [Route("api/Employee/Create")]
+        public int Create(Employee employee)
+        {
+            return objemployee.AddEmployee(employee);
+        }
+
+        [HttpGet]
+        [Route("api/Employee/Details/{id}")]
+        public Employee Details(int id)
+        {
+            return objemployee.GetEmployeeData(id);
+        }
+
+        [HttpPut]
+        [Route("api/Employee/Edit")]
+        public int Edit(Employee employee)
+        {
+            return objemployee.UpdateEmployee(employee);
+        }
+
+        [HttpDelete]
+        [Route("api/Employee/Delete/{id}")]
+        public int Delete(int id)
+        {
+            return objemployee.DeleteEmployee(id);
+        }
+        public List<Employee> Get()
+        {
+            var employees = new List<Employee>();
+
+            var connectionStringBuilder = new SqliteConnectionStringBuilder() { DataSource = "./SqliteDB.db" };
+            using (var connection = new SqliteConnection(connectionStringBuilder.ConnectionString))
+            {
+                connection.Open();
+
+                var queryCmd = connection.CreateCommand();
+                queryCmd.CommandText = @"SELECT Name, Value FROM Employees";
+                using (var reader = queryCmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        employees.Add(new Employee
+                        {
+                            Name = reader.GetString(0),
+                            Value = reader.GetInt32(1)
+                        });
+                    }
+                }
+            }
+
+            return employees;
+        }
+    }
+}
